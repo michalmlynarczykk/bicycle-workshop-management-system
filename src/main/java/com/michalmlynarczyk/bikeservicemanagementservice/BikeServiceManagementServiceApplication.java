7be88b57b0a777d90eced01a592dff5b44980976
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2023-08-07 21:59:13 +0200

    initial commit

---
+++ src/main/java/com/michalmlynarczyk/bikeservicemanagementservice/BikeServiceManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.michalmlynarczyk.bikeservicemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeServiceManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeServiceManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/michalmlynarczyk/bikeservicemanagementservice/BikeServiceManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalmlynarczyk.bikeservicemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeServiceManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
