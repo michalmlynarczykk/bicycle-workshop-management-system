cd179d28052ccda8c2c362d909d8aaa87080ce24
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2023-11-20 23:19:34 +0100

    add restcontroller annotation to orderController

--- src/main/java/com/michalmlynarczyk/bikeservicemanagementservice/controller/OrderControllerImpl.java
+++ src/main/java/com/michalmlynarczyk/bikeservicemanagementservice/controller/OrderControllerImpl.java
@@ -1,7 +1,9 @@
 package com.michalmlynarczyk.bikeservicemanagementservice.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class OrderControllerImpl implements OrderController {
 
     @Override
