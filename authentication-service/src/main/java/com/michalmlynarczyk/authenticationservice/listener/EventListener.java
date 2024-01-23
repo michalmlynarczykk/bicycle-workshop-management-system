32be7bfa4aba66a7571c9d8ab77cd42e5141bf97
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2024-01-23 22:46:07 +0100

    fix rabbit and brokerClient configuration

--- authentication-service/src/main/java/com/michalmlynarczyk/authenticationservice/listener/EventListener.java
+++ authentication-service/src/main/java/com/michalmlynarczyk/authenticationservice/listener/EventListener.java
@@ -2,8 +2,12 @@ package com.michalmlynarczyk.authenticationservice.listener;
 
 import com.michalmlynarczyk.common.model.dto.broker.BrokerMessageWrapper;
 import com.michalmlynarczyk.common.model.dto.broker.workshop.WorkshopCreatedEvent;
+import jakarta.validation.Valid;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public interface EventListener {
 
-    void receiveWorkshopCreatedEvent(BrokerMessageWrapper<WorkshopCreatedEvent> message);
+    void receiveWorkshopCreatedEvent(@Payload @Valid final BrokerMessageWrapper<WorkshopCreatedEvent> message);
 }
--- workshop-management-service/src/main/java/com/michalmlynarczyk/workshopmanagementservice/client/broker/BrokerClient.java
+++ workshop-management-service/src/main/java/com/michalmlynarczyk/workshopmanagementservice/client/broker/BrokerClient.java
@@ -2,10 +2,13 @@ package com.michalmlynarczyk.workshopmanagementservice.client.broker;
 
 import com.michalmlynarczyk.common.model.dto.broker.workshop.WorkshopCreatedEvent;
 import com.michalmlynarczyk.common.model.dto.broker.workshop.WorkshopJoinApplicationApprovedEvent;
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public interface BrokerClient {
 
-    void notifyAboutWorkshopCreated(final WorkshopCreatedEvent payload);
+    void notifyAboutWorkshopCreated(@Valid final WorkshopCreatedEvent payload);
 
-    void notifyAboutWorkshopJoinApplicationApproved(final WorkshopJoinApplicationApprovedEvent payload);
+    void notifyAboutWorkshopJoinApplicationApproved(@Valid final WorkshopJoinApplicationApprovedEvent payload);
 }
